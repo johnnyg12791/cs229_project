6efb83d2365ea289b8778dd1e356dded52ee3ffa
John Gold <johnnyg12791@gmail.com>
2014-11-10 14:15:05 -0800

    sanitizing data and basic python starter

--- trial1.py
+++ trial1.py
@@ -1,1 +1,17 @@
 #Our first attempt at real life ML...
+
+
+
+
+def main():
+  print "test"
+  ReadFileInAsMatrix()
+  #[team, avg_margin]
+  #[team2, avg_margin..]...345]
+
+
+def ReadFileInAsMatrix():
+  
+
+if __name__ == "__main__":
+  main()
