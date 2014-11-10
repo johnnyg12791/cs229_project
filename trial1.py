7cc5cd47eb7c380845fe5af3e127defbca65c368
Elliot Chanen <echanen@dn0a22710a.sunet>
2014-11-10 15:24:37 -0800

    beginning making matrices

--- trial1.py
+++ trial1.py
@@ -1,17 +1,28 @@
 #Our first attempt at real life ML...
+import csv
 
 
-
+def ReadFileInAsMatrix(file1):
+    '''with open(file1, 'rb') as csvfile:
+        filereader = csv.reader(csvfile)
+        for row in filereader:
+            print row
+            #print ' '.join(row)
+            '''
+    content = open(file1).read().splitlines()
+    for row in content:
+        print row.split(",")
 
 def main():
-  print "test"
-  ReadFileInAsMatrix()
-  #[team, avg_margin]
-  #[team2, avg_margin..]...345]
+    print "test"
+    ReadFileInAsMatrix('data/sports-reference/2012_margin_of_victory.csv')
 
+        #'data/kaggle/teams.csv')
 
-def ReadFileInAsMatrix():
+    # data/sports-reference/2012_margin_of_victory.csv
+
+    #[team, avg_margin]
+    #[team2, avg_margin..]...345]
   
-
 if __name__ == "__main__":
-  main()
+    main()
