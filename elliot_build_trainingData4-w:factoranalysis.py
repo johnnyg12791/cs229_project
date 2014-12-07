34e4f7259b89c2f371b37fba50cec1aa40b7884b
Elliot Chanen <echanen@dn51s41g.sunet>
2014-12-06 21:41:32 -0800

    finished 2013 bracket

--- elliot_build_trainingData4-w:factoranalysis.py
+++ elliot_build_trainingData4-w:factoranalysis.py
@@ -13,6 +13,7 @@ from sklearn.cross_validation import cross_val_score
 
 
 global team_name_to_id
+global id_to_team_name
 global factors_to_results
 #global games_matrix
 
@@ -20,15 +21,15 @@ def main():
     BuildTeamNameToIdMap('data/kaggle/teams.csv')
     #1 = id, 5 = W/L %, 6 = SRS, 18 = FG%, 21 = 3PFG%, 24 = FT%, 32 = Avg Margin
     #33 = TRB/G, 34 = AST/G, 35 = TOV/G, 36 = FLS/G, 37 = STl/G, 38 = Blk/G
-    basic_stats_matrix = ReadBasicStatsToMatrix('data/sports-reference/2014_basic_stats.csv', [1, 5, 6, 18, 21, 24, 32, 33, 34, 35, 36, 37, 38]) #
-    regular_season_games = BuildGamesMatrix('data/kaggle/regular_season_results.csv', 85863, 91225)
+    basic_stats_matrix = ReadBasicStatsToMatrix('data/sports-reference/basic_stats.csv', [1, 5, 6, 18, 21, 24, 32, 33, 34, 35, 36, 37, 38]) #
+    regular_season_games = BuildGamesMatrix('data/kaggle/regular_season_results.csv', 80543, 85863)
     #print regular_season_games
     #print games_matrix
 
     #So now I have the 3 different data sources into numpy matricies
     #combine the games matrix and stats matrix
     training_data = buildTrainingDataMatrix(basic_stats_matrix, regular_season_games, 12)
-    print training_data
+    #print training_data
     (X,y) = (training_data[:,range(24)], training_data[:,24]) #
     #print X
     #print y
@@ -43,21 +44,31 @@ def main():
     #print model2.predict([1.75, .12]) #look, we think team 1 will lose
     #print model.predict_proba([.55, .76, .34, .52]) #we think team 1 will win
 
-    ###tournament_games = BuildGamesMatrix('data/kaggle/tourney_results.csv', 1024, 1090)
+    tournament_games = BuildGamesMatrix('data/kaggle/tourney_results.csv', 1090, 1158)
+    #print tournament_games
+    print len(tournament_games)
     
     #test on tourny and test on regular season
-    ###test_data = buildTrainingDataMatrix(basic_stats_matrix, tournament_games, 12)
+    test_data = buildTrainingDataMatrix(basic_stats_matrix, tournament_games, 12)
     #test_data = buildTrainingDataMatrix(basic_stats_matrix, regular_season_games)
     #this is training_data!!!!
 
-    #print len(test_data) only get 32 of the games due to naming....
-    """predictions = MakePredictions(model, test_data[:,range(24)]) #
+    print len(test_data) #only get 32 of the games due to naming....
+    predictions = MakePredictions(model, test_data[:,range(24)]) #
+    #print predictions
+    print len(predictions)
+    for index,predict in enumerate(predictions):
+        if (predict == 1):
+            print (id_to_team_name[int(tournament_games[index][0])], id_to_team_name[int(tournament_games[index][1])])
+        else:
+            print (id_to_team_name[int(tournament_games[index][1])], id_to_team_name[int(tournament_games[index][0])])
+
     accuracy = GetAccuracy(predictions, test_data, 12)
     print "LogisticRegression Accuracy: ", accuracy
 
     predictions2 = MakePredictionsSVM(model2, test_data[:,range(24)])
     accuracy2 = GetAccuracy(predictions2, test_data, 12)
-    print "SVM Accuracy: ", accuracy2"""
+    print "SVM Accuracy: ", accuracy2
     #FactorAnalysis()
 
 
@@ -123,7 +134,7 @@ def GetAccuracy(predictions, test_data, numFac):
     correct_predictions = 0
     for index, prediction in enumerate(predictions):
         if(prediction == test_data[:,2*numFac][index]): #
-            correct_predictions += 1.0
+            correct_predictions += 1.0    
     return correct_predictions / float(len(predictions))
 
 #Predictions for an SVM
@@ -141,6 +152,7 @@ def MakePredictionsSVM(model, test_data):
 #Predictions using a classification
 def MakePredictions(model, test_data):
     predictions = []
+    print len(test_data)
     for data in test_data:
         predict = model.predict_proba(data)
         if(predict[0][0] > .5):
@@ -211,9 +223,9 @@ def buildTrainingDataMatrix(stats_matrix, games_matrix, numFac):
     
     #Remove rows that didn't correspond to a team (naming issues between datasets)
     rows_to_delete = []
-    for row_index, data_row in enumerate(training_data_matrix):
+    """for row_index, data_row in enumerate(training_data_matrix):
         if(data_row[0] == 0 or data_row[numFac] == 0): #
-            rows_to_delete.append(row_index)
+            rows_to_delete.append(row_index)"""
 
     #now return the training data with non-filled rows deleted
     return np.delete(training_data_matrix, rows_to_delete, 0) #delete the rows going backwards
@@ -255,11 +267,14 @@ Takes the given file (kaggle teams), and puts it into a map from name to id
 '''
 def BuildTeamNameToIdMap(filename):
     global team_name_to_id
+    global id_to_team_name
     team_name_to_id = {}
+    id_to_team_name = {}
     content = open(filename).read().splitlines()[1:]
     for row in content:
         row_split = row.split(",")
         team_name_to_id[row_split[1]] = int(row_split[0])
+        id_to_team_name[int(row_split[0])] = row_split[1]
 
 
 
